05623c3ea9ccfbd10f11ecf5433086c8a2c4ad3d
니엘 <129924943+nyeri9999@users.noreply.github.com>
2026-03-30 14:58:24 +0900

    4week hw upload
    4week hw upload

---
+++ code/4week/4week-chap3-실습.sql
@@ -0,0 +1,40 @@
+-- SQL 작성 --
+-- 1. 테이블 생성
+-- 유저
+CREATE TABLE users (
+                       user_id SERIAL PRIMARY KEY,
+                       username VARCHAR(30) NOT NULL,
+                       email VARCHAR(50) UNIQUE NOT NULL,
+                       join_date DATE DEFAULT CURRENT_DATE
+);
+-- 게임 프로필
+CREATE TABLE game_profile (
+                              profile_id SERIAL PRIMARY KEY,
+                              user_id INT UNIQUE NOT NULL,
+                              level INT DEFAULT 1,
+                              gold INT DEFAULT 0,
+                              play_time INT DEFAULT 0,
+                              rank_tier VARCHAR(20),
+                              CONSTRAINT fk_user
+                                  FOREIGN KEY (user_id)
+                                      REFERENCES users(user_id)
+                                      ON DELETE CASCADE
+);
+-- 2. 데이터 삽입
+-- 유저
+INSERT INTO users (username, email)
+VALUES
+    ('player1', 'player1@test.com'),
+    ('player2', 'player2@test.com');
+-- 게임 프로필
+INSERT INTO game_profile (user_id, level, gold, play_time, rank_tier)
+VALUES
+    (1, 15, 3000, 120, 'Gold'),
+    (2, 20, 5000, 200, 'Platinum');
+-- 엔티티별 전체 조회
+SELECT * FROM users;
+SELECT * FROM game_profile;
+-- 레벨 20 이상 유저 조회
+SELECT * FROM game_profile WHERE level >= 20;
+-- 게임머니 순으로 정렬(desc)
+SELECT * FROM game_profile ORDER BY gold DESC;
